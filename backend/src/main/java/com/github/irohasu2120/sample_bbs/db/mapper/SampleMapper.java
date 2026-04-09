5cda05adc7d7d623e00505521caba34d9e13a3de
irohasu2120 <sa1122photo@yahoo.co.jp>
2026-04-10 07:15:31 +0900

    fix: MyBatisDynamicSQLのサンプル完成

--- backend/src/main/java/com/github/irohasu2120/sample_bbs/db/mapper/SampleMapper.java
+++ backend/src/main/java/com/github/irohasu2120/sample_bbs/db/mapper/SampleMapper.java
@@ -2,12 +2,15 @@ package com.github.irohasu2120.sample_bbs.db.mapper;
 
 import com.github.irohasu2120.sample_bbs.db.domain.Sample;
 import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.dynamic.sql.util.mybatis3.CommonCountMapper;
-import org.mybatis.dynamic.sql.util.mybatis3.CommonDeleteMapper;
-import org.mybatis.dynamic.sql.util.mybatis3.CommonInsertMapper;
-import org.mybatis.dynamic.sql.util.mybatis3.CommonSelectMapper;
-import org.mybatis.dynamic.sql.util.mybatis3.CommonUpdateMapper;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
+import org.mybatis.dynamic.sql.util.SqlProviderAdapter;
+
+import java.util.List;
 
 @Mapper
-public interface SampleMapper extends CommonSelectMapper, CommonCountMapper, CommonDeleteMapper, CommonInsertMapper<Sample>, CommonUpdateMapper {
+public interface SampleMapper {
+    @SelectProvider(type = SqlProviderAdapter.class, method = "select")
+//    @ResultMap("Sample")
+    List<Sample> selectMany(SelectStatementProvider provider);
 }
