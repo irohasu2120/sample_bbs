b1f22c21d47b6d8497231e03614f9933d0e72a73
irohasu2120 <sa1122photo@yahoo.co.jp>
2026-04-10 07:14:11 +0900

    fix: MyBatisDynamicSQLのサンプル完成

--- backend/src/main/java/com/github/irohasu2120/sample_bbs/sample/SampleController.java
+++ backend/src/main/java/com/github/irohasu2120/sample_bbs/sample/SampleController.java
@@ -4,11 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * サンプル Controller
+ */
 @RestController
 @RequiredArgsConstructor
 public class SampleController {
     private final SampleService sampleService;
 
+    /**
+     * サンプル Getリクエスト
+     * @return 実行結果
+     */
     @GetMapping("/hello")
     public String sampleGet() {
         return sampleService.mainRoutine();
--- backend/src/main/java/com/github/irohasu2120/sample_bbs/sample/SampleService.java
+++ backend/src/main/java/com/github/irohasu2120/sample_bbs/sample/SampleService.java
@@ -1,30 +1,46 @@
 package com.github.irohasu2120.sample_bbs.sample;
 
 
+import com.github.irohasu2120.sample_bbs.db.domain.Sample;
 import com.github.irohasu2120.sample_bbs.db.domain.SampleDSLSupport;
 import com.github.irohasu2120.sample_bbs.db.mapper.SampleMapper;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.mybatis.dynamic.sql.render.RenderingStrategies;
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.List;
 
 import static org.mybatis.dynamic.sql.SqlBuilder.isEqualTo;
 import static org.mybatis.dynamic.sql.SqlBuilder.select;
 
+/**
+ * サンプル Service
+ */
 @Service
 @RequiredArgsConstructor
 public class SampleService {
-
     private final SampleMapper sampleMapper;
 
+    /**
+     * Service
+     * @return 実行結果
+     */
     public String mainRoutine() {
-        Map<String, Object> result =  sampleMapper.selectOneMappedRow(generateFindOneCondition(1));
-        return ((String)result.get("name"));
+        SelectStatementProvider stmt = getSelectStatementProvider();
+        List<Sample> result = sampleMapper.selectMany(stmt);
+        return result.getFirst().getName();
     }
 
-    private SelectStatementProvider generateFindOneCondition(Integer id) {
-        return select(SampleDSLSupport.sample.allColumns()).from(SampleDSLSupport.sample).where(SampleDSLSupport.id, isEqualTo(id)).build().render(RenderingStrategies.MYBATIS3);
+    /**
+     * 検索SQL Statementの生成
+     * @return 検索SQLStatement
+     */
+    private static @NonNull SelectStatementProvider getSelectStatementProvider() {
+        return select(SampleDSLSupport.sample.allColumns())
+                .from(SampleDSLSupport.sample)
+                .where(SampleDSLSupport.sample.id, isEqualTo(1))
+                .build().render(RenderingStrategies.MYBATIS3);
     }
 }
