3fca4ac0d48478f2310472988ae83fa1c71a3737
irohasu2120 <sa1122photo@yahoo.co.jp>
2026-04-11 09:46:46 +0900

    add: flywayの追加とSpringBootを3.5系に落とした。Spring Securityも導入した

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "4.0.5"
+	id("org.springframework.boot") version "3.5.13"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -24,29 +24,47 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	compileOnly("org.projectlombok:lombok:1.18.44")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.projectlombok:lombok:1.18.44")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	// Spring Boot
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// Spring Boot (test)
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	// DB
+	// Spring Boot (dev only)
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// lombok
+	compileOnly("org.projectlombok:lombok:1.18.44")
+	annotationProcessor("org.projectlombok:lombok:1.18.44")
+
+	// Spring Boot (DB)
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
+	// DB (Driver)
+//	implementation("com.mysql:mysql-connector-j:9.6.0")
+	runtimeOnly("com.mysql:mysql-connector-j:9.6.0")
+
+	// DB (MyBatis)
 	implementation("org.mybatis:mybatis:3.5.19")
-	implementation("com.mysql:mysql-connector-j:9.6.0")
-	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:4.0.1")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.5")
 	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:2.0.0")
 //	implementation("org.mybatis.generator:mybatis-generator-core:1.4.2")
+
+	// DB (flyway)
+	implementation("org.flywaydb:flyway-core:11.14.1")
+	implementation("org.flywaydb:flyway-mysql:11.14.1")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
 //
-//tasks.register("mybatisGenerator") {
-//	doLast {
-//		org.mybatis.generator.api.ShellRunner.main(
-//
-//		)
-//	}
+//flyway {
+//	url = "jdbc:mysql://localhost:3306/appdb?useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=UTF-8&serverTimezone=Asia/Tokyo"
+//	user = "app_user"
+//	password = "app_pass"
+//	driver = "com.mysql.cj.jdbc.Driver"
 //}
